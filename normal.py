5795fec0e4705e66fc8a239e69b2eb9d445e941f
Gregor Kovač <79193306+gregorkovac@users.noreply.github.com>
2024-04-29 16:58:21 +0200

    Premature version of std normal integral

---
+++ normal.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+def std_normal(x):
+    return 1 / np.sqrt(2 * np.pi) * np.exp(-x**2 / 2)
+
+def std_normal_integral(x, n=1000):
+    # TODO: Fix this. Maybe with variable substitution.
+    a = x - 1000
+    b = x
+
+    h = (b - a) / n
+
+    t_vals = np.linspace(a, b, n + 1)
+    f_vals = std_normal(t_vals)
+
+    integral = h * (np.sum(f_vals) - 0.5 * (f_vals[0] + f_vals[-1]))
+
+    return integral
