c1838c3dae388f96d0461e58b4a824b1aab54623
Gregor Kovač <79193306+gregorkovac@users.noreply.github.com>
2024-05-08 19:04:50 +0200

    Transformacija normal integrala na [-1, 1]

--- normal.py
+++ normal.py
@@ -6,6 +6,14 @@ std_normal(x): vrne vrednost standardne normalne porazdelitve v točki x
 def std_normal(x):
     return 1 / np.sqrt(2 * np.pi) * np.exp(-x**2 / 2)
 
+def transf(x):
+    return np.tanh(x)
+
+def inv_transf(x):
+    x[x == -1] = -1 + 1e-11
+    x[x == 1] = 1 - 1e-11
+    return np.arctanh(x)
+
 """
 std_normal_integral(x, n, lower_limit): vrne aproksimacijo integrala standardne normalne porazdelitve na intervalu [-neskončno, x] z uporabo trapeznega pravila
 
@@ -16,17 +24,17 @@ Vhod:
 
 Izhod: (float) aproksimacija integrala
 """
-def std_normal_integral(x, n=1000000, lower_limit=-10, upper_limit=10):
-    # Nastavimo meje integrala. Pazimo, da nimamo -inf ali inf
-    a = lower_limit
-    b = max(min(x, upper_limit), lower_limit)
+def std_normal_integral(x, n=1000000):
+
+    a = -1
+    b = transf(x)
 
     # Izračunamo dolžino koraka
     h = (b - a) / n
 
     # Izračunamo vrednosti funkcije v točkah
     t_vals = np.linspace(a, b, n + 1)
-    f_vals = std_normal(t_vals)
+    f_vals = std_normal(inv_transf(t_vals)) / (1 - t_vals**2 + 1e-20)
 
     # Izračunamo integral s trapeznim pravilom
     integral = h * (np.sum(f_vals) - 0.5 * (f_vals[0] + f_vals[-1]))
